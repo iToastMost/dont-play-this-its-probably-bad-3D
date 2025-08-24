1dfdcdd5bda21fd3553bc8d0453385f27b8499d2
iToastMost <chevre94@gmail.com>
2025-08-24 13:08:31 -0400

    fixed input issues with GooseJump not registering controls and added an animation for the 'phone' sliding in and out of the scene

--- Scripts/PlayerRE.cs
+++ Scripts/PlayerRE.cs
@@ -13,16 +13,32 @@ public partial class PlayerRE : CharacterBody3D
     public float turnSpeed = 4f;
 
     private Vector3 _targetVelocity = Vector3.Zero;
-    private SubViewportContainer _subViewportContainer;
+    private SubViewport _subViewport;
+    private PackedScene _gooseJumpScene;
+
+    private AnimationPlayer _phoneAnimation;
+    private bool _phoneVisible = false;
+    Node GooseScene;
     public override void _Ready()
     {
-        _subViewportContainer = GetNode<SubViewportContainer>("SubViewportContainer");
+        _phoneAnimation = GetNode<AnimationPlayer>("SubViewportContainer/AnimationPlayer");
+        _subViewport = GetNode<SubViewport>("SubViewportContainer/SubViewport");
+        _gooseJumpScene = ResourceLoader.Load<PackedScene>("res://GooseJump/Scenes/main.tscn");
     }
 
     public override void _PhysicsProcess(double delta)
     {
         Move(delta);
         MoveAndSlide();
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (_phoneVisible) 
+        {
+            _subViewport?.PushInput(@event);
+            GetViewport().SetInputAsHandled();
+        }
     }
 
     private void Move(double delta) 
@@ -48,15 +64,32 @@ public partial class PlayerRE : CharacterBody3D
         {
             RotateY(-turnSpeed * (float)delta);
         }
+        
         if (Input.IsActionJustPressed("open_phone")) 
         {
-            if (_subViewportContainer.Visible == true) 
+            if (_phoneVisible == true) 
             {
-                _subViewportContainer.Visible = false;
+                //_subViewportContainer.Visible = false;
+                _phoneVisible = false;
+                _phoneAnimation.CurrentAnimation = "slide_in";
+                _phoneAnimation.PlayBackwards();
+                
+                
+                //GooseScene.QueueFree();
             }
             else 
             {
-                _subViewportContainer.Visible = true;
+                //_subViewportContainer.Visible = true;
+                _phoneVisible = true;
+                //if(GooseScene == null) 
+                {
+                    //GooseScene = _gooseJumpScene.Instantiate();
+                    //_subViewport.AddChild(GooseScene);
+                }
+                //GooseScene = _gooseJumpScene.Instantiate();
+                //_subViewport.AddChild(GooseScene);
+                _phoneAnimation.CurrentAnimation = "slide_in";
+                _phoneAnimation.Play();
             }
         }
 
