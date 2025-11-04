34effc14dc77eb6d89ee6dc8bdcdefdd994432d4
iToastMost <chevre94@gmail.com>
2025-11-03 21:18:22 -0500

    added more cameras, refridgerators and washers/dryers to the showroom floor. Also switched default IDE to rider

--- Scripts/CameraSwitcher.cs
+++ Scripts/CameraSwitcher.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 
+[Tool]
 public partial class CameraSwitcher : Area3D
 {
     [Export]
@@ -9,9 +10,17 @@ public partial class CameraSwitcher : Area3D
     [Export]
     private Camera3D _nextCamera;
 
+    [Export] private double sizeX;
+    [Export] private double sizeY;
+    [Export] private double sizeZ;
+    
+    CollisionShape3D _collisionShape;
+
     public override void _Ready()
     {
         BodyEntered += OnAreaEnter;
+        _collisionShape = GetNode<CollisionShape3D>("CollisionShape3D");
+        
     }
 
     public void OnAreaEnter(Node3D body) 
