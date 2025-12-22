0f98c5b815714bd8d09e71a13cf520b2edfee8ef
iToastMost <chevre94@gmail.com>
2025-12-22 18:56:59 -0500

    Added enemy wander state but they move throgh collision boxes, most likely because MoveAndSlide() is not in the wander state class

--- Scripts/FiniteStateMachine/EnemyStateMachine/EnemyStates/WanderState.cs
+++ Scripts/FiniteStateMachine/EnemyStateMachine/EnemyStates/WanderState.cs
@@ -4,10 +4,13 @@ using System.Collections.Generic;
 
 public class WanderState : EnemyState
 {
+	private Vector3 _targetLocation;
+	
 	private List<RayCast3D> _rayCasts;
 	public override void Enter()
 	{
 		_rayCasts = _enemy.GetRayCasts();
+		_targetLocation = GenerateRandomLocation();
 	}
 
 	public override void PhysicsUpdate(double delta)
@@ -16,20 +19,46 @@ public class WanderState : EnemyState
 		{
 			if (node is RayCast3D ray)
 			{
-				if (!ray.IsColliding())
-					return;
-				
 				if (ray.GetCollider() is PlayerRE)
 				{ 
 					_enemyStateMachine.ChangeState(EnemyStateTypes.Chase);
 				}
-					
 			}
 		}
+		
+		MoveToLocation(_targetLocation, delta);
 	}
 
 	public override void Exit()
 	{
 		
+	}
+
+	private Vector3 GenerateRandomLocation()
+	{
+		var rng = new Random();
+		var randX = rng.NextInt64(-10, 10);
+		var randY = rng.NextInt64(-10, 10);
+		
+		var currentLocation = _enemy.GlobalPosition;
+		var newLocation = currentLocation;
+		newLocation.X = newLocation.X + randX;
+		newLocation.Z = newLocation.Z + randY;
+		
+		return newLocation;
+	}
+
+	private void MoveToLocation(Vector3 location, double delta)
+	{
+		if (_enemy.GlobalPosition.DistanceTo(location) < 0.5)
+		{
+			_targetLocation = GenerateRandomLocation();
+		}
+		
+		var direction = Vector3.Zero;
+		var playerDirection = (_enemy.GlobalPosition - location);
+		
+		direction = playerDirection.Normalized() / 25f;
+		_enemy.Position -= direction * (float)delta * _enemy._enemyMoveSpeed;
 	}
 }
