90eb7a262b9f46f25f2f71f3a94b9564738ce9d2
Brandon Hayes <chevre94@gmail.com>
2026-01-19 21:44:17 -0500

    changed around where items are in the game

--- Scripts/Npc.cs
+++ Scripts/Npc.cs
@@ -8,13 +8,16 @@ public partial class Npc : CharacterBody3D
 	
 	[Export]
 	public string NPCDialogue { get; set; }
+	
+	[Export]
+	public string StartingAnimation { get; set; }
 
 	private AnimationPlayer _animationPlayer;
 	
 	public override void _Ready()
 	{
 		_animationPlayer = GetNode<AnimationPlayer>("NPC_Model/AnimationPlayer");
-		_animationPlayer.CurrentAnimation = "Idle";
-		_animationPlayer.Play("Idle");
+		_animationPlayer.CurrentAnimation = StartingAnimation;
+		_animationPlayer.Play(StartingAnimation);
 	}
 }
